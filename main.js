0deeac3157c52fdc8989c6614aef906a3c5ea472
ArolaunTech <137230773+ArolaunTech@users.noreply.github.com>
2023-07-02 11:42:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -6,9 +6,3 @@ console.log('trajCanvas:');
 console.log(trajCanvas);
 
 var gltraj = trajCanvas.getContext('webgl2');
-
-//Error throw
-if (!gltraj) {
-  alert("Your browser doesn't appear to support WebGL.");
-  throw "Your browser doesn't appear to support WebGL.";
-}
