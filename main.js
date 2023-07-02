39409f3c3b34a90ba40d102643104c60bed00bf5
ArolaunTech <137230773+ArolaunTech@users.noreply.github.com>
2023-07-02 11:37:09 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 //Main js file
 
 //GL context
-var canvas = document.getElementById('');
+var trajCanvas = document.getElementById('trajectory-display');
+console.log('trajCanvas:');
+console.log(trajCanvas);
+
+var gltraj = trajCanvas.getContext('webgl2');
+
+//Error throw
+if (!gltraj) {
+  alert('Your browser doesn't appear to support WebGL.');
+  throw('Your browser doesn't appear to support WebGL.');
+}
