818fec1331aa1faa8abd939b28b445a8e23109a8
ArolaunTech <137230773+ArolaunTech@users.noreply.github.com>
2024-01-03 13:35:12 -0800

    New "space" background

--- javascripts/main.js
+++ javascripts/main.js
@@ -227,7 +227,7 @@ function animate(time) {
 	trajgl.viewport(0,0,trajgl.canvas.width, trajgl.canvas.height);
 	trajgl.clear(trajgl.COLOR_BUFFER_BIT | trajgl.DEPTH_BUFFER_BIT);
 	trajgl.enable(trajgl.DEPTH_TEST);
-	trajgl.clearColor(0,0,1,1);
+	trajgl.clearColor(0.055,0.071,0.102,1);
 
 	//Request next frame
 	window.requestAnimationFrame(animate);
