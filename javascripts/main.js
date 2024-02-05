cf80712b6dc4f016e4f908d5780c19079895751b
ArolaunTech <137230773+ArolaunTech@users.noreply.github.com>
2024-02-04 20:10:03 -0800

    Sphere rendering test 5

--- javascripts/main.js
+++ javascripts/main.js
@@ -225,8 +225,10 @@ void main() {
 var planetFragmentShader = `#version 300 es
 precision mediump float;
 
+out vec4 fragColor;
+
 void main() {
-	gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+	fragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }`;
 
 function makeProgram(gl, vertexShaderSource, fragmentShaderSource) {
